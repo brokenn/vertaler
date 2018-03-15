a85467720c10c838e5e603294b3c07be2f92231c
Brian O'Kennedy <brokenn@gmail.com>
2018-03-15 22:41:51 +0000

    run and run_prompt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <streambuf>
 
-int main()
+void run (const std::string & code)
 {
-    std::cout << "Hello world" << std::endl;
+    std::cout << "RUN: " << code << std::endl;
+}
+
+int run_prompt ()
+{
+    for (std::string line; std::getline (std::cin, line); )
+    {
+        run (line);
+    } 
+    return 0;
+}
+
+int run_file (const std::string & filename)
+{
+    std::ifstream is (filename);
+    std::string text ((std::istreambuf_iterator<char> (is)),
+                       std::istreambuf_iterator<char> ());
+    run (text);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        return run_prompt ();
+    }
+    else if (argc == 2)
+    {
+        return run_file (argv[1]);
+    }
+    else
+    {
+        std::cerr << "Incorrect use" << std::endl;
+    }
 }
 
